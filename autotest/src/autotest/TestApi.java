c8983d3324c5674765f1d8c8185768c5bdfa2cad
wutao2000 <erik20130312@163.com>
2017-09-19 00:08:15 +0800

    new one

---
+++ autotest/src/autotest/TestApi.java
@@ -0,0 +1,10 @@
+package autotest;
+
+public class TestApi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("·Ü¶·µÄÈËÉú£¡£¡£¡£¡£¡£¡");
+	}
+
+}
