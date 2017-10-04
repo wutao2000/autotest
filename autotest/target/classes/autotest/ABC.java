7852045b240ac4c18e2abd52dfc82e01b01d7308
wutao2000 <erik20130312@163.com>
2017-10-04 21:15:16 +0800

    ok

---
+++ autotest/target/classes/autotest/ABC.java
@@ -0,0 +1,12 @@
+package autotest;
+
+public class ABC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("wo shi !!!!!");
+        
+        
+	}
+
+}
